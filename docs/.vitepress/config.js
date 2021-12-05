5f8d9783dcc1c46738a4aaee1d96d158ba2cd2d1
nbn <nbn1209@126.com>
2021-12-05 21:48:19 +0800

    feat: [更新]

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -22,27 +22,36 @@ module.exports = {
       // 侧边栏
       "/": [
         {
-          text: "介绍",
+          text: "手册",
+        
           children: [
-            { text: "什么是react-ant-admin？", link: "/" },
-            { text: "开始使用", link: "/guide/start" },
-            { text: "文件配置", link: "/guide/configuration" },
-            { text: "路径配置", link: "/guide/path" },
+            { text: "基础", link: "/handbook/basics" },
+            { text: "日常类型", link: "/handbook/everyday-types" },
+            { text: "函数", link: "/handbook/more-on-functions" },
+           
           ],
         },
+        {
+          text: '参考资料',
+         
+          children: [
+            { text: "实用类型", link: "/reference/utility-types" }
+          ]
+        }
       ],
+     
     },
     nav: [
-      // 顶部右侧导航栏
-      { text: "介绍", link: "/", activeMatch: "^/$|^/guide/" },
-      {
-        text: "预览地址",
-        link: "https://azhengpersonalblog.top/react-ant-admin/",
-      },
-      {
-        text: "更多地址",
-        link: "/contact/",
-      },
+      // // 顶部右侧导航栏
+      // { text: "介绍", link: "/", activeMatch: "^/$|^/guide/" },
+      // {
+      //   text: "预览地址",
+      //   link: "https://azhengpersonalblog.top/react-ant-admin/",
+      // },
+      // {
+      //   text: "更多地址",
+      //   link: "/contact/",
+      // },
     ],
   },
 }
